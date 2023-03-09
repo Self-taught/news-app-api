83b471b81a8ccb7f43f7789c36df344b36ba9d35
Self-taught <63093667+Self-taught@users.noreply.github.com>
2023-03-09 13:29:02 +0530

    final code

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ def send_email(message, receiver):
     host = "smtp.gmail.com"
     port = 465
 
-    username = "looftee08@gmail.com"
-    password = "fclhtgzwyjwbnxar"
+    username = "Your Configured Gmail"
+    password = "Your Api Key"
 
     context = ssl.create_default_context()
 
@@ -16,13 +16,14 @@ def send_email(message, receiver):
         server.sendmail(username, receiver, message)
 
 
-api_key = "654527ebf9044394afafc1eb04005366"
+api_key = "Your Api Key"
 
 url = ('https://newsapi.org/v2/everything?'
        'q=cryptocurrency, altcoins, bitcoin, ethereum&'
        'from=2023-03-01&'
        'sortBy=popularity&'
-       'apiKey=654527ebf9044394afafc1eb04005366')
+       'language=en&'
+       'apiKey=Your Api Key')
 
 # Make request
 response = requests.get(url)
@@ -36,21 +37,14 @@ with st.form(key="form"):
     button = st.form_submit_button("Get email")
     if button:
         body = ""
-        a = 0
-        for art in content["articles"]:
-            if a <= 10:
-                body = body + art["title"] + "\n" + art["description"] + 2*"\n"
+        for art in content["articles"][:10]:
+            body = body + art["title"] + "\n" + art["description"] + "\n" + art["url"] + 2*"\n"
 
         body = body.encode("utf-8")
         send_email(body, receiver_email)
         st.info("Email Sent")
 # Access the article title and description
-a = 0
-for art in content["articles"]:
-    if a <= 10:
-        st.header(art["title"])
-        st.write(art["description"])
-        st.markdown(f"[Source]({art['url']})")
-        a += 1
-    else:
-        break
+for art in content["articles"][:10]:
+    st.header(art["title"])
+    st.write(art["description"])
+    st.markdown(f"[Source]({art['url']})")
